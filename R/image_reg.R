7df9be5143d9cddf2b6132f4da84ae89309e29f6
Martin Wilson <martin@pipegrep.co.uk>
2017-06-13 09:37:59 +0100

    checks on image compatability

--- R/image_reg.R
+++ R/image_reg.R
@@ -1,16 +1,15 @@
 #' @export
-get_svs_voi <- function(mrs_data, target_mri = NA) {
+get_svs_voi <- function(mrs_data, target_mri) {
   affine <- get_mrs_affine(mrs_data)
   raw_data <- array(1, c(mrs_data$resolution[2:4]))
   voi <- RNifti::retrieveNifti(raw_data)
   voi <- RNifti::`sform<-`(voi, structure(affine, code = 2L))
   
-  if (is.na(target_mri)) {
+  if (missing(target_mri)) {
     warning("Target MRI data has not been specified.")  
   } else {
     voi <- resample_voi(voi, target_mri)
   }
-  
   voi  
 }
 
@@ -22,33 +21,70 @@ resample_voi <- function(voi, mri) {
 
 #' @export
 plot_svs_overlay <- function(svs_voi, mri_data) {
-  # TODO check the image orientation etc is the same
+  # check the image orientation etc is the same
+  check_geom(svs_voi, mri_data)
+  
   # get the centre of gravity coords
   vox_inds <- get_voi_cog(svs_voi)
   plot_col <- add_alpha(grDevices::heat.colors(10), 0.4)
-  neurobase::ortho2(oro.nifti::nifti(mri_data), oro.nifti::nifti(svs_voi),
-                    xyz = vox_inds, col.y = plot_col, zlim.y = c(1, 2))
+  mri_oro <- neurobase::robust_window(oro.nifti::nifti(mri_data))
+  neurobase::ortho2(mri_oro, oro.nifti::nifti(svs_voi), xyz = vox_inds, 
+                    col.y = plot_col, zlim.y = c(1, 2))
 }
 
 #' @export
-plot_svs_overlay_seg <- function(svs_voi, mri_data) {
-  # TODO check the image orientation etc is the same
+plot_svs_overlay_seg <- function(svs_voi, seg_data) {
+  # check the image orientation etc is the same
+  check_geom(svs_voi, seg_data)
+  
   # get the centre of gravity coords
   vox_inds <- get_voi_cog(svs_voi)
   
-  pvs <- get_voi_seg(svs_voi, mri_data)
+  pvs <- get_voi_seg(svs_voi, seg_data)
   table <- paste("WM\t\t=  ", sprintf("%.1f", pvs[["WM"]]), "%\nGM\t\t=  ", 
                  sprintf("%.1f", pvs[["GM"]]), "%\nCSF\t=  ", 
                  sprintf("%.1f", pvs[["CSF"]]), "%\nOther\t=  ", 
                  sprintf("%.1f", pvs[["Other"]]),'%', sep = "")
   
   plot_col <- add_alpha(grDevices::heat.colors(10), 0.4)
-  neurobase::ortho2(oro.nifti::nifti(mri_data), oro.nifti::nifti(svs_voi),
+  neurobase::ortho2(oro.nifti::nifti(seg_data), oro.nifti::nifti(svs_voi),
                     xyz = vox_inds, col.y = plot_col, zlim.y = c(1, 2))
   
   graphics::par(xpd = NA)
   graphics::text(x = 0.55, y = 0.12, labels = c(table), col = "white", pos = 4, 
                  cex = 1.5)
+}
+
+#' @export
+apply_pvc <- function(result, pvcs, tr){
+  te = result$data$te
+  B0 = round(result$data$ft / 42.58e6,1)
+  corr_factor <- get_corr_factor(te, tr, B0, pvcs[["GM"]], pvcs[["WM"]],
+                                 pvcs[["CSF"]])
+  
+  amp_cols = result$amp_cols
+  default_factor = 35880 * 0.7
+  result$results$GM_vol = pvcs[["GM"]]
+  result$results$WM_vol = pvcs[["WM"]]
+  result$results$CSF_vol = pvcs[["CSF"]]
+  result$results$Other_vol = pvcs[["Other"]]
+  result$results_pvc <- result$results
+  
+  # append tables with %GM, %WM, %CSF and %Other
+  pvc_cols <- 6:(5 + amp_cols * 2)
+  result$results_pvc[, pvc_cols] <- result$results_pvc[, pvc_cols] /
+                                    default_factor * corr_factor
+  return(result)
+}
+
+#' @export
+get_voi_seg <- function(voi, seg_data) {
+  # check the image orientation etc is the same
+  check_geom(voi, seg_data)
+  vals <- seg_data[voi == 1]
+  pvs <- summary(factor(vals, levels = c(0, 1, 2, 3), 
+        labels = c("Other", "CSF", "GM", "WM"))) / sum(voi) * 100
+  return(pvs)
 }
 
 # generate an sform affine for nifti generation
@@ -73,44 +109,18 @@ get_mrs_affine <- function(mrs_data) {
   return(affine)
 }
 
-#' @export
-get_vox_pvcs <- function(mrs_data, seg_mri) {
-  mrs_nii <- get_svs_voi(mrs_data)
-  res_mrs <- resample_voi(mrs_nii, seg_mri)
-  pvs <- get_voi_seg(res_mrs, seg_mri)
-  return(pvs)  
-}
+# check two nifti images are in the same space
+check_geom <- function(a, b) {
+  eq_xform <- identical(xform(a), xform(b))
+  eq_dim <- identical(dim(a), dim(b))
+  eq_pix_dim <- identical(pixdim(a), pixdim(b))
+  
+  if ( !eq_xform | !eq_dim | !eq_pix_dim ) {
+    stop("Inconsistant image geometry.")
+  }
+} 
 
-#' @export
-apply_pvc <- function(result, pvcs, tr){
-  te = result$data$te
-  B0 = round(result$data$ft / 42.58e6,1)
-  corr_factor <- get_corr_factor(te, tr, B0, pvcs[["GM"]], pvcs[["WM"]],
-                                 pvcs[["CSF"]])
-  
-  amp_cols = result$amp_cols
-  default_factor = 35880 * 0.7
-  result$results$GM_vol = pvcs[["GM"]]
-  result$results$WM_vol = pvcs[["WM"]]
-  result$results$CSF_vol = pvcs[["CSF"]]
-  result$results$Other_vol = pvcs[["Other"]]
-  result$results_pvc <- result$results
-  pvc_cols <- 6:(5 + amp_cols * 2)
-  result$results_pvc[, pvc_cols] <- result$results_pvc[, pvc_cols] /
-                                    default_factor * corr_factor
-  
-  # append tables with %GM, %WM, %CSF and %Other
-  
-  return(result)
-}
-
+# VOI centre of gravity
 get_voi_cog <- function(voi) {
   as.integer(colMeans(which(voi == 1, arr.ind = TRUE)))
-}
-
-get_voi_seg <- function(voi, seg_data) {
-  vals <- seg_data[voi == 1]
-  pvs <- summary(factor(vals, levels = c(0, 1, 2, 3), 
-        labels = c("Other", "CSF", "GM", "WM"))) / sum(voi) * 100
-  return(pvs)
 }
