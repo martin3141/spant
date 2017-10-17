5052ec3fe7e51b263d4ea4b7490347f8377e1fb4
Martin Wilson <martin@pipegrep.co.uk>
2017-10-17 09:12:07 +0100

    added basic fitting test

---
+++ tests/testthat/test_fitting.R
@@ -0,0 +1,11 @@
+context("fitting")
+
+test_that("a full fitting pipeline works", {
+  fname <- system.file("extdata", "philips_spar_sdat_WS.SDAT", package = "spant")
+  mrs_data <- read_mrs(fname, format = "spar_sdat")
+  mrs_proc <- hsvd_filt(mrs_data)
+  mrs_proc <- align(mrs_proc, 2.01)
+  basis <- sim_basis_1h_brain_press(mrs_proc)
+  fit_res <- fit_mrs(mrs_proc, basis)
+  expect_equal_to_reference(fit_res, "fit_res.rds")
+})
