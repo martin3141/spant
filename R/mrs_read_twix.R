baea3533e41e1a4d11d8f6e2d42f12703c907108
Martin Wilson <martin@pipegrep.co.uk>
2018-01-22 09:04:34 +0000

    working on twix reader

--- R/mrs_read_twix.R
+++ R/mrs_read_twix.R
@@ -4,39 +4,71 @@ read_twix <- function(fname) {
   con <- file(fname, "rb")
   first_int <- read_uint32(con)
   second_int <- read_uint32(con)
-  print(first_int)
-  print(second_int)
+  #print(first_int)
+  #print(second_int)
   if ((first_int < 10000) && (second_int <= 64)) {
-    cat("File is VD format.\n")
+    cat("Twix file is VD format.\n")
     version <- "vd"
     Nscans <- second_int
     measID <- read_uint32(con)
-    print(measID)
+    #print(measID)
     fileID <- read_uint32(con)
-    print(fileID)
+    #print(fileID)
     measOffset <- read_uint64(con) # usually 10240 bytes
-    print(measOffset)
+    #print(measOffset)
     measLength <- read_uint64(con)
     hdrLength <- read_uint32(con)
     dataStart <- measOffset + hdrLength
-    print(dataStart)
+    #print(dataStart)
   } else {
-    cat("File is VB format.\n")
+    cat("Twix file is VB format.\n")
     version <- "vb"
     dataStart <- first_int
     Nscans <- 1
   }
   close(con)
+  vars <- read_twix_txt_hdr(fname, version)
+}
+
+read_twix_txt_hdr <- function(fname, version) {
+  con <- file(fname, 'rb', encoding = "UTF-8")
+  while (TRUE) {
+    line <- readLines(con, n = 1 ,skipNul = TRUE)
+    if (startsWith(line, "ulVersion") && version == 'vb') {
+      break
+    }
+    
+    if (startsWith(line, "ulVersion") && version == 'vd') {
+      tSequenceFilename <- readLines(con, n = 1)
+      tProtocolName <- readLines(con, n = 1)
+      if ((tProtocolName != "tProtocolName\t = \t\"AdjCoilSens\"") && (tProtocolName != "tProtocolName\t = \t\"CBU_MPRAGE_32chn\"")) {
+        #print(tProtocolName)
+        break
+      }
+    }
+  }
   
-  # read the text header line by line
-  con = file(fname, "rb")
-  x = 0 
-  while (x < 100) {
-    x <- x + 1
-    line = readLines(con, n = 1, skipNul = TRUE)
-    print(line)
+  vars <- vector(mode = "list", length = 5)
+  names(vars) <- c("averages", "fs", "ft", "te", "N")
+  
+  while (TRUE) {
+    line <- readLines(con, n = 1, skipNul = TRUE)
+    if (grepl("### ASCCONV END ###", line, fixed = TRUE)) {
+      break
+    } else if (startsWith(line, "lAverages")) {
+      vars$averages <- as.integer(strsplit(line, "=")[[1]][2])
+    } else if (startsWith(line, "sRXSPEC.alDwellTime[0]")) {
+      vars$fs <- 1e9 / (as.numeric(strsplit(line, "=")[[1]][2]))
+    } else if (startsWith(line, "sTXSPEC.asNucleusInfo[0].lFrequency")) {
+      vars$ft <- as.numeric(strsplit(line, "=")[[1]][2])
+    } else if (startsWith(line, "alTE[0]")) {
+      vars$te <- (as.numeric(strsplit(line, "=")[[1]][2])) / 1e6
+    } else if (startsWith(line, "sSpecPara.lVectorSize")) {
+      vars$N <- as.integer(strsplit(line, "=")[[1]][2])
+    }
   }
   close(con)
+  vars
 }
 
 read_float32 <- function(con) {
@@ -69,44 +101,4 @@ read_uint64 <- function(con) {
   # warning - R doesn't nativly support 64 bit integers - so this will be 
   # an approximation by converting to double
   sum(2^.subset(0:63, as.logical(raw_bits)))
-}
-
-read_twix_txt_hdr <- function(fname, version) {
-  con <- file(fname, 'rb', encoding = "UTF-8")
-  while (TRUE) {
-    line <- readLines(con, n = 1 ,skipNul = TRUE)
-    if (startsWith(line, "ulVersion") && version == 'vb') {
-      break
-    }
-    
-    if (startsWith(line, "ulVersion") && version == 'vd') {
-      tSequenceFilename <- readLines(con, n = 1)
-      tProtocolName <- readLines(con, n = 1)
-      if ((tProtocolName != 'tProtocolName\t = \t"AdjCoilSens"\n') && (tProtocolName != 'tProtocolName\t = \t"CBU_MPRAGE_32chn"\n')) {
-        break
-      }
-    }
-  }
-  
-  vars <- vector(mode = "list", length = 5)
-  names(vars) <- c("averages", "fs", "ft", "te", "N")
-  
-  while (TRUE) {
-    line <- readLines(con, n = 1, skipNul = TRUE)
-    if (grepl("### ASCCONV END ###", line, fixed = TRUE)) {
-      break
-    } else if (startsWith(line, "lAverages")) {
-      vars$averages <- as.integer(strsplit(line, "=")[[1]][2])
-    } else if (startsWith(line, "sRXSPEC.alDwellTime[0]")) {
-      vars$fs <- 1e9 / (as.numeric(strsplit(line, "=")[[1]][2]))
-    } else if (startsWith(line, "sTXSPEC.asNucleusInfo[0].lFrequency")) {
-      vars$ft <- as.numeric(strsplit(line, "=")[[1]][2])
-    } else if (startsWith(line, "alTE[0]")) {
-      vars$te <- (as.numeric(strsplit(line, "=")[[1]][2])) / 1e6
-    } else if (startsWith(line, "sSpecPara.lVectorSize")) {
-      vars$N <- as.integer(strsplit(line, "=")[[1]][2])
-    }
-  }
-  close(con)
-  vars
 }
