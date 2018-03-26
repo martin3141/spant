4420d05351ea5a587f9152efb07eb4e921e830c5
Martin Wilson <martin@pipegrep.co.uk>
2018-03-26 12:54:50 +0100

    tol change to qm simualation tests

--- tests/testthat/test_qm_sim.R
+++ tests/testthat/test_qm_sim.R
@@ -1,5 +1,6 @@
 context("qm simulator")
 
 test_that("default basis is consistant", {
-  expect_equal_to_reference(sim_basis_1h_brain_press(), "def_basis.rds")
+  expect_equal_to_reference(sim_basis_1h_brain_press(), "def_basis.rds",
+                            tolerance = 1e-6)
 })
