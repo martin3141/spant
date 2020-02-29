b8c0b1667b4f27e5a92003bf86b0b1720af2ab6c
Martin Wilson <martin@pipegrep.co.uk>
2020-02-29 16:23:28 +0000

    added another test

--- tests/testthat/test_fitting.R
+++ tests/testthat/test_fitting.R
@@ -1,5 +1,21 @@
 context("fitting")
 
+test_that("ABfit no-optim works", {
+  fname <- system.file("extdata", "philips_spar_sdat_WS.SDAT",
+                       package = "spant")
+  
+  mrs_data <- read_mrs(fname, format = "spar_sdat")
+  basis    <- sim_basis_1h_brain_press(mrs_data)
+  
+  # don't run any optimisation steps
+  opts <- abfit_opts(maxiters_pre = 0, maxiters = 0)
+  fit_res  <- fit_mrs(mrs_data, basis, method = "abfit", opts = opts,
+                      time = FALSE)
+  
+  expect_equal_to_reference(fit_res, "fit_res_abfit_no_optim.rds",
+                            tolerance = 1e-4)
+})
+
 test_that("ABfit pre-fitting works", {
   fname <- system.file("extdata", "philips_spar_sdat_WS.SDAT",
                        package = "spant")
