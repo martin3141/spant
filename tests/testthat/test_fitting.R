1c915023061edca6dca07187658d08e28c9232b3
Martin Wilson <martin@pipegrep.co.uk>
2020-02-29 16:06:22 +0000

    simpler abfit test

--- tests/testthat/test_fitting.R
+++ tests/testthat/test_fitting.R
@@ -5,10 +5,8 @@ test_that("a full fitting pipeline works", {
                        package = "spant")
   
   mrs_data <- read_mrs(fname, format = "spar_sdat")
-  mrs_proc <- hsvd_filt(mrs_data)
-  mrs_proc <- align(mrs_proc, 2.01)
-  basis    <- sim_basis_1h_brain_press(mrs_proc)
+  basis    <- sim_basis_1h_brain_press(mrs_data)
   
-  fit_res  <- fit_mrs(mrs_proc, basis, method = "abfit", time = FALSE)
+  fit_res  <- fit_mrs(mrs_data, basis, method = "abfit", time = FALSE)
   expect_equal_to_reference(fit_res, "fit_res_abfit.rds", tolerance = 1e-4)
 })
