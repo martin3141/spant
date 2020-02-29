e5b95d2e067ec976f4ef17d1cc8c1a6ac18dc518
Martin Wilson <martin@pipegrep.co.uk>
2020-02-29 16:16:47 +0000

    new test

--- tests/testthat/test_fitting.R
+++ tests/testthat/test_fitting.R
@@ -1,6 +1,21 @@
 context("fitting")
 
-test_that("a full fitting pipeline works", {
+test_that("ABfit pre-fitting works", {
+  fname <- system.file("extdata", "philips_spar_sdat_WS.SDAT",
+                       package = "spant")
+  
+  mrs_data <- read_mrs(fname, format = "spar_sdat")
+  basis    <- sim_basis_1h_brain_press(mrs_data)
+  
+  # don't run the fine fitting step
+  opts <- abfit_opts(maxiters = 0)
+  fit_res  <- fit_mrs(mrs_data, basis, method = "abfit", opts = opts,
+                      time = FALSE)
+  
+  expect_equal_to_reference(fit_res, "fit_res_abfit_pre.rds", tolerance = 1e-4)
+})
+
+test_that("ABfit full works", {
   fname <- system.file("extdata", "philips_spar_sdat_WS.SDAT",
                        package = "spant")
   
