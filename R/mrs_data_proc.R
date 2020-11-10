0f6203270d9d602c123ea9f257595212f6d51418
Martin Wilson <martin@pipegrep.co.uk>
2020-11-10 10:56:36 +0000

    new option for extrapolation

--- R/mrs_data_proc.R
+++ R/mrs_data_proc.R
@@ -2727,25 +2727,38 @@ bc_als_vec <- function(vec, lambda, p) {
 
 #' Back extrapolate time-domain points using the Burg autoregressive model
 #' @param mrs_data mrs_data object.
-#' @param n_pts number of points to extrapolate.
+#' @param extrap_pts number of points to extrapolate.
+#' @param pred_pts number of points to base the extrapolation on.
 #' @return back extrapolated data.
 #' @export
-back_extrap <- function(mrs_data, n_pts) {
+back_extrap <- function(mrs_data, extrap_pts, pred_pts = NA) {
   
   if (is_fd(mrs_data)) mrs_data <- fd2td(mrs_data)
   
   Np <- Npts(mrs_data) 
+  
   mrs_data$data <- mrs_data$data[,,,,,,Np:1, drop = FALSE]
-  mrs_data <- apply_mrs(mrs_data, 7, back_extrap_vec, n_pts)
-  mrs_data$data <- mrs_data$data[,,,,,,(Np + n_pts):1, drop = FALSE]
+  mrs_data <- apply_mrs(mrs_data, 7, back_extrap_vec, extrap_pts, pred_pts)
+  mrs_data$data <- mrs_data$data[,,,,,,(Np + extrap_pts):1, drop = FALSE]
   mrs_data
 }
 
-back_extrap_vec <- function(vec, n_pts) {
-  new_pts_re <- as.numeric(stats::predict(stats::ar.burg(Re(vec)),
-                                          se.fit = FALSE, n.ahead = n_pts))
-  new_pts_im <- as.numeric(stats::predict(stats::ar.burg(Im(vec)),
-                                          se.fit = FALSE, n.ahead = n_pts))
+back_extrap_vec <- function(vec, extrap_pts, pred_pts) {
+  
+  # use the full vector unless instructed otherwise
+  if (is.na(pred_pts)) {
+    pred_vec <- vec 
+  } else {
+    pred_vec <- tail(vec, pred_pts)
+  }
+  
+  new_pts_re <- as.numeric(stats::predict(stats::ar.burg(Re(pred_vec),
+                                                         demean = TRUE),
+                                          se.fit = FALSE, n.ahead = extrap_pts))
+  
+  new_pts_im <- as.numeric(stats::predict(stats::ar.burg(Im(pred_vec),
+                                                         demean = TRUE),
+                                          se.fit = FALSE, n.ahead = extrap_pts))
   c(vec, new_pts_re + new_pts_im * 1i)
 }
 
