be5eaec62cb38aa7c879d8608c3c16e088f26e91
Martin Wilson <martin@pipegrep.co.uk>
2020-12-11 11:56:13 +0000

    dicom reader improvements

--- R/dicom_reader.R
+++ R/dicom_reader.R
@@ -7,15 +7,17 @@ read_raw <- function(fname, endian = "little") {
   return(readBin(fname, "raw", n = as.integer(fsize), endian = endian))
 }
 
-# A very simple DICOM reader. tags is a named list of the tag data to be
 # extracted from the file eg tags <- list(spec_data = "7FE1,1010",
 #                                         pat_name = "0010,0010")
 # A named list is returned containing the raw binary data is returned.
-dicom_reader <- function(input, tags) {
+#' @export
+dicom_reader <- function(input, tags = list(sop_class_uid = "0008,0016"),
+                         endian = "little", debug = FALSE) {
   
   # assume as file path if input is character, otherwise assume raw
   if (is.character(input)) {
-    fraw <- read_raw(input)
+    fsize <- file.info(input)$size
+    fraw <- readBin(input, "raw", n = as.integer(fsize), endian = endian)
   } else {
     fraw <- input 
   }
@@ -23,11 +25,12 @@ dicom_reader <- function(input, tags) {
   # skip the 128 byte preamble and read the next 4 bytes (should be "DICM")
   dicom_prefix <- rawToChar(fraw[129:132])
   
-  if (dicom_prefix != "DICM") stop("File is not DICOM.")
+  if (dicom_prefix != "DICM") {
+    stop("Could not find DICM prefix - input is probally not DICOM.")
+  }
   
-  fsize_orig <- length(fraw)
   # strip the preamble
-  fraw <- fraw[133:fsize_orig]
+  fraw <- fraw[133:length(fraw)]
   
   # prepare the output structure
   out <- tags
@@ -40,15 +43,20 @@ dicom_reader <- function(input, tags) {
   # current position in the data
   pos <- 0
   
+  if (debug) debug_table <- NULL
+  
   while (pos < length(fraw)) {
     
     # read the tag ID
     group   <- rawToHex(fraw[pos + 1:2])
     element <- rawToHex(fraw[pos + 3:4])
+    tag_str <- paste(group, element, sep = ",") 
     
     # tentatively read in the VR - only used for explicit VRs
     vr <- rawToChar(fraw[pos + 5:6])
-    if (vr %in% short_vrs) {
+    
+    # 5600,0020 is a hack that seems to be needed for Siemens SpectroscopyData
+    if (vr %in% short_vrs | tag_str == "5600,0020") {
       # explicit VR with two bytes of zero padding
       length_raw <- fraw[pos + 9:12]
       length <- read_uint32(length_raw)
@@ -66,19 +74,33 @@ dicom_reader <- function(input, tags) {
       pos <- pos + 8
     }
     
-    tag_str <- paste(group, element, sep = ",") 
+    if (rawToHex(length_raw) %in% c("FFFFFFFF", "00000A00")) length <- 0 
+      
+    if (length == 0) {
+      start_byte <- NA
+      end_byte   <- NA
+    } else {
+      start_byte <- pos + 1
+      end_byte   <- pos + length
+    }
+  
+    if (debug) {
+      tag <- paste("(", tag_str, ")", sep = "")
+      x <- data.frame(tag, start_byte, end_byte, length)
+      debug_table <- rbind(debug_table, x)
+    }
     
     # check if current tag is the in list of those to be returned
     search_res <- grep(tag_str, tags) 
     if (length(search_res) == 1) {
-      start_byte <- pos + 1
-      end_byte   <- pos + length
       out[[search_res]] <- fraw[start_byte:end_byte]
     }
     
     pos <- pos + length
     
-    # cat(paste(group, element, start_byte, end_byte, length, "\n"))
   }
+  
+  if (debug) print(debug_table)
+  
   return(out)
 }
