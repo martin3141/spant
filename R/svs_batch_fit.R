ff69319a64546f7bb45f3171a341f307339108d9
Martin Wilson <martin@pipegrep.co.uk>
2021-01-27 14:52:44 +0000

    batch processing

--- R/svs_batch_fit.R
+++ R/svs_batch_fit.R
@@ -8,8 +8,23 @@ svs_1h_analysis <- function(metab, basis, w_ref = NULL, mri_seg = NULL,
                             scale_amps = TRUE, te = NULL, tr = NULL,
                             output_dir = NULL) {
   
-  ws_path <- as.character(ws_path)  # possibly needed to stop issues when
+  # TODO check metab is not an mrs_data object first
+  # metab <- as.character(metab)    # possibly needed to stop issues when
                                     # cbinding fit tables later on
+ 
+  # read the data file if not already an mrs_data object
+  if (class(metab) != "mrs_data") metab <- read_mrs(metab)
+  
+  # read the ref data file if not already an mrs_data object
+  if (is.def(w_ref) & (class(w_ref) != "mrs_data")) w_ref <- read_mrs(w_ref)
+  
+  if (is.def(mri) & (!("niftiImage" %in% class(mri)))) mri <- readNifti(mri)
+  
+  if (is.def(mri_seg) & (!("niftiImage" %in% class(mri_seg)))) {
+    mri_seg <- readNifti(mri_seg)
+  }
+  
+  # TODO reorient images?
   
   # combine coils if needed
   if (Ncoils(metab) > 1) {
@@ -77,49 +92,43 @@ svs_1h_analysis <- function(metab, basis, w_ref = NULL, mri_seg = NULL,
 # add extra option for id vars
 
 #' @export
-svs_1h_batch_analysis <- function(metab_paths, w_ref_paths = NULL,
-                                  mri_paths = NULL, mri_seg_paths = NULL,
-                                  output_dirs = NULL, ...) {
+svs_1h_batch_analysis <- function(metab_list, w_ref_list = NULL,
+                                  mri_list = NULL, mri_seg_list = NULL,
+                                  output_dir = NULL, ...) {
   
   # check input it sensible
-  metab_n <- length(metab_paths)
+  metab_n <- length(metab_list)
   
-  if (!is.null(w_ref_paths) & length(w_ref_paths) != metab_n) {
-    stop("Incorrect number of ref items.")
+  if (is.def(w_ref_list) & length(w_ref_list) != metab_n) {
+    stop("Incorrect number of w_ref_list items.")
   }
   
-  if (!is.null(mri_paths) & length(mri_paths) != metab_n) {
-    stop("Incorrect number of mri items.")
+  if (is.def(mri_list) & length(mri_list) != metab_n) {
+    stop("Incorrect number of mri_list items.")
   }
   
-  if (!is.null(mri_seg_paths) & length(mri_seg_paths) != metab_n) {
-    stop("Incorrect number of mri_seg items.")
+  if (is.def(mri_seg_list) & length(mri_seg_list) != metab_n) {
+    stop("Incorrect number of mri_seg_list items.")
   }
   
-  if (!is.null(output_dirs) & length(output_dirs) != metab_n) {
-    stop("Incorrect number of output_dir items.")
+  if (is.def(output_dir) & length(output_dir) != metab_n) {
+    stop("Incorrect number of output_dir_list items.")
   }
   
   # check file paths exist TODO write a function to return bad paths
-  # metab_paths[which(!file.exists(metab_paths))]
   
-  # convert file paths to a list of mrs_data objects
-  metab_list <- lapply(metab_paths, read_mrs)
+  if (is.null(w_ref_list)) w_ref_list <- vector("list", metab_n)
   
-  if (is.null(w_ref_paths)) {
-    w_ref_list <- vector("list", metab_n)
-  } else {
-    w_ref_list <- lapply(w_ref_paths, read_mrs)
-  }
+  if (is.null(mri_seg_list)) mri_seg_list <- vector("list", metab_n)
   
-  if (is.null(mri_seg_paths)) {
-    mri_seg_list <- vector("list", metab_n)
-  } else {
-    mri_seg_list <- lapply(mri_seg_paths, readNifti)
-  }
+  if (is.null(mri_list)) mri_list <- vector("list", metab_n)
+  
+  if (is.null(output_dir)) output_dir_list <- vector("list", metab_n)
   
   fit_list <- mapply(svs_1h_analysis, metab = metab_list, w_ref = w_ref_list,
-                     mri_seg = mri_seg_list, MoreArgs = list(...), SIMPLIFY = FALSE)
+                     mri_seg = mri_seg_list, mri = mri_list,
+                     output_dir = output_dir_list,
+                     MoreArgs = list(...), SIMPLIFY = FALSE)
   
   return(fit_list)
 }
