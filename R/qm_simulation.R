2b0b00dab4bc8980591d4627a496d273114615ca
Martin Wilson <martin@pipegrep.co.uk>
2022-12-16 14:59:36 +0000

    NA fix

--- R/qm_simulation.R
+++ R/qm_simulation.R
@@ -235,6 +235,8 @@ apply_pulse <- function(sys, rho, spin_n, angle, nuc, xy) {
   
   if (nuc != sys$nucleus[spin_n]) return(rho)
   
+  if (is.na(angle)) return(rho)
+  
   if (angle < 1) return(rho)
   
   F <- gen_I(spin_n, sys$spin_num, xy)
