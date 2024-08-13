661f10d34fd641405da5534adc5aff0f70d15acb
Martin Wilson <martin@pipegrep.co.uk>
2024-08-13 08:27:14 +0100

    revert limits

--- R/abfit.R
+++ R/abfit.R
@@ -348,12 +348,14 @@ abfit <- function(y, acq_paras, basis, opts = NULL) {
       max_basis_dampings[broad_indices] <- opts$max_basis_damping_broad
     }
     
+    # opts$max_shift * acq_paras$ft * 1e-6
+    
     upper <- c(opts$max_phase * pi / 180, opts$max_damping,
-               res$par[3] + opts$max_shift * acq_paras$ft * 1e-6, opts$max_asym,
+               res$par[3] + opts$max_shift, opts$max_asym,
                max_basis_shifts, max_basis_dampings)
     
     lower <- c(-opts$max_phase * pi / 180, 0,
-               res$par[3] - opts$max_shift * acq_paras$ft * 1e-6,
+               res$par[3] - opts$max_shift,
                -opts$max_asym, -max_basis_shifts, rep(0, Nbasis))
     
     if (opts$phi1_optim) {
