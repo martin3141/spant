985b7cc66dc87abb7a738db1eaf9d0c54a21aaf1
Martin Wilson <martin@pipegrep.co.uk>
2025-02-10 15:07:46 +0000

    new fn

--- R/cli_utils.R
+++ R/cli_utils.R
@@ -20,4 +20,23 @@ install_cli <- function(path = NULL) {
   
   # make executable
   Sys.chmod(fit_svs_path, mode = "755")
+  
+  cat("spant_fit_svs sucessfully installed to : ", path, "\n", sep = "")
+}
+
+# https://www.r-bloggers.com/2015/06/identifying-the-os-from-r/
+get_os <- function() {
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)) {
+    os <- sysinf['sysname']
+    if (os == 'Darwin')
+      os <- "macos"
+  } else { ## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os))
+      os <- "macos"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "linux"
+  }
+  tolower(os)
 }
