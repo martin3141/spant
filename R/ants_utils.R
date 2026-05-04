185399a1056643f1b3a535b7c2d6dfc235bc27ea
Martin Wilson <martin.wilson111@gmail.com>
2026-05-04 11:29:05 +0100

    doc update

--- R/ants_utils.R
+++ R/ants_utils.R
@@ -17,8 +17,9 @@ get_spant_resources_dir <- function() {
 
 #' Install ANTs / ANTsX from : https://github.com/ANTsX/ANTs/releases
 #' @param platform see the releases page for supported platforms. Common
-#' platforms include : "macos-14-ARM64-clang", "centos7-X64-gcc" or 
-#' "ubuntu-24.04-X64-gcc".
+#' platforms include : "macos-14-ARM64-clang", "almalinux9-X64-gcc",
+#' "centos7-X64-gcc" or "ubuntu-24.04-X64-gcc". Note, It may be necessary to
+#' increase the timeout with "options(timeout = 1000)" on slower connections.
 #' @param version ANTs version to install, defaults to "2.6.5".
 #' @export
 install_ants <- function(platform, version = "2.6.5") {
